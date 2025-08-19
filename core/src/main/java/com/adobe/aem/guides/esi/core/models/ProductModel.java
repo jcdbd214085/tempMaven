7d7e929f7401badb7153f1bdd4d048732d02b203
jaycee.zheng <jcdbd214085@gmail.com>
2025-08-19 11:48:01 +0800

    新增演示用的模板

---
+++ core/src/main/java/com/adobe/aem/guides/esi/core/models/ProductModel.java
@@ -0,0 +1,45 @@
+package com.adobe.aem.guides.esi.core.models;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class ProductModel {
+
+    @ValueMapValue(name = "./productTitle")
+    private String productTitle;
+
+    @ValueMapValue(name = "./productInfo")
+    private String productInfo;
+
+    @ValueMapValue(name = "./fileReference")
+    private String fileReference;
+
+    @ValueMapValue(name = "./productDate")
+    private Date productDate;
+
+
+    public String getProductTitle() {
+        return productTitle;
+    }
+
+    public String getProductInfo() {
+        return productInfo;
+    }
+
+    public String getFileReference() {
+        return fileReference;
+    }
+
+    public String getFormattedProductDate() {
+        if (productDate != null) {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            return sdf.format(productDate);
+        }
+        return null;
+    }
+}
