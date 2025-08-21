a3d456b13f83bce5afb9fe3248adfad020867b94
jaycee.zheng <jcdbd214085@gmail.com>
2025-08-21 15:35:54 +0800

    新增靜態Servlet

---
+++ core/src/main/java/com/adobe/aem/guides/esi/core/servlets/ProductServlet.java
@@ -0,0 +1,31 @@
+package com.adobe.aem.guides.esi.core.servlets;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.Servlet;
+import java.io.IOException;
+
+@Component(service = {Servlet.class},
+        property = {
+                "sling.servlet.paths=/bin/esi-practice/product",
+                "sling.servlet.methods=GET",
+                "sling.servlet.extensions=json"
+        })
+//http://localhost:4502/bin/esi-practice/product.json
+public class ProductServlet extends SimpleServlet {
+    private static final Logger LOG = LoggerFactory.getLogger(ProductServlet.class);
+
+    @Override
+    protected void doGet(SlingHttpServletRequest req, SlingHttpServletResponse resp) throws IOException {
+
+        LOG.info("Processing GET request for /bin/esi-practice/product");
+        resp.setContentType("application/json");
+        resp.getWriter().write("{\"message\": \"我來自ProductServlet API呼叫\"}");
+    }
+
+}
